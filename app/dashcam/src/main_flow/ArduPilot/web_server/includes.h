9b2201eae138590300a73dc612e866fa9ccc4d86
Peter Barker <pbarker@barker.dropbear.id.au>
2017-10-28 13:59:49 +1100

    ArduPilot: web-server: include.h: linux support

--- app/dashcam/src/main_flow/ArduPilot/web_server/includes.h
+++ app/dashcam/src/main_flow/ArduPilot/web_server/includes.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#ifdef SYSTEM_FREERTOS
 #include <FreeRTOS.h>
 #include <bsp.h>
 #include <task.h>
@@ -17,6 +18,10 @@
 #include "../talloc.h"
 #include "../dev_console.h"
 #include "../util/print_vprintf.h"
+#else
+#include "linux/includes.h"
+#include "mavlink_core.h"
+#endif
 
 #include "cgi.h"
 #include "template.h"
